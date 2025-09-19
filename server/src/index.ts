eae5a3f2f8c7800af85ffbbea9ea13cb83ba9021
Volodymyr <PropertyChernivtsy@gmail.com>
2025-09-19 21:05:15 +0200

    0.0.0.0

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import { AppDataSource } from "./utils/DataSource";
 
 
 
-const port = process.env.PORT || 8000;
+const port:number = Number(process.env.PORT) || 8000;
 const HOST = process.env.HOST || "localhost";
 console.log("HOST", HOST, port);
 
@@ -19,6 +19,9 @@ AppDataSource.initialize()
         console.log("DB connected ✅");
         //await seedUsers();
         const server = http.createServer(app);
-        server.listen(port, ()=> {console.log(`🚀 Server running at http://${HOST}:${port}`)});
+        //server.listen(port, ()=> {console.log(`🚀 Server running at http://${HOST}:${port}`)});
+        server.listen(port, "0.0.0.0", () => {
+            console.log(`🚀 Server running on port ${port}`);
+        });
     })
     .catch((err) => console.error("DB connection error: ", err));
