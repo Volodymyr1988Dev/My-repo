6d34b1c8b17d201284a8a98a98e7d78c9e762323
Volodymyr <PropertyChernivtsy@gmail.com>
2025-09-26 18:38:40 +0200

    fixed login register path

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -13,10 +13,13 @@ const Login:React.FC<LoginProps> = ({ setToken }) => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
+ const VITE_API_URL = import.meta.env.VITE_API_URL as string;
+ 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+
     try {
-      const res = await fetch('/api/login', {
+      const res = await fetch(`${VITE_API_URL}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
@@ -27,10 +30,11 @@ const Login:React.FC<LoginProps> = ({ setToken }) => {
         throw new Error(errData.message || 'Login failed');
       }
       const data = await res.json();
-     // Login(data.token);
+     
       sessionStorage.setItem('token', data.token);
       setToken(data.token);
       console.log('Logged in with token:', data.token);
+      console.log('data:', data);
       navigate('/posts');
     } catch (err: unknown) {
       if (!(err instanceof Error)) {
