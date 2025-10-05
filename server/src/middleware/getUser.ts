e2d86c5d505549a1e7282a7b52496af41a01c22d
Volodymyr <PropertyChernivtsy@gmail.com>
2025-10-05 07:57:21 +0200

    UserSevice.ts

--- server/src/middleware/getUser.ts
+++ server/src/middleware/getUser.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
+interface AuthRequest extends Request {
+  user?: { id: number; email: string };
+}
 
-
-export function getUserHandler(req: Request, res: Response) {
+export function getUserHandler(req: AuthRequest, res: Response) {
   res.json({ user: req.user });
 }
